253eafc22485c306f36a8cdbcdcd46ca0a078e79
Pepe <bonmatip@gmail.com>
2022-03-01 21:35:05 +0100

    versión comentada

--- geometria/src/Rectangulo_JB.java
+++ geometria/src/Rectangulo_JB.java
@@ -1,14 +1,24 @@
 
+/**
+ * @author Jose A. Bonmatí Garrido
+ * @version 1.2
+ *
+ */
 public class Rectangulo_JB extends FiguraGeometrica_JB {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * @param tipoFigura El tipo de figura
+	 * @param lG El lado grande del rectangulo
+	 * @param lP El lado pequeño del rectangulo
+	 */
 	public Rectangulo_JB(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+	
 	@Override
 	public double area() {
 		return l1 * l2;
